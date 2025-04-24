f8375eca36d6b97c6665343dd8b552a7860dd39e
Ruan Diego <Ruandgn123@gmail.com>
2025-04-24 12:45:53 -0300

    feat: chatbot com respostas prontas

---
+++ lading-page/src/components/chat.tsx
@@ -0,0 +1,63 @@
+import { normalizeText } from "@/utils/nomalizeText";
+import { useState } from "react";
+import { TeamData } from "../models/TeamData";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+
+interface ChatProps {
+    escolha: string;
+    handleChat: (item: string) => void;
+}
+
+const infoTeam = [
+     'Camisa','Integrantes', 'Títulos', 'Situação', 'Voltar'
+]
+
+
+
+export function Chat({ escolha, handleChat }: ChatProps) {
+    const [infoTeamEscolhida, setInfoTeamEscolhida] = useState<string>();
+
+    function handleInfoTeam(item: string) {
+        if(item == 'Voltar'){
+            handleChat('');
+        } else {
+            const escolhaTeam = TeamData[escolha as keyof typeof TeamData];
+            const value = escolhaTeam[normalizeText(item) as keyof typeof escolhaTeam];
+            setInfoTeamEscolhida(value);
+        }        
+    }
+
+    return (
+        <div>
+            <div className="flex items-center gap-2">
+                <div className='flex items-center gap-2 p-3 rounded-lg bg-gray-800 mr-8'>
+                    <Avatar>
+                        <AvatarFallback>RD</AvatarFallback>
+                        <AvatarImage src="https://cdn.dribbble.com/userupload/11627402/file/original-519eba43b5e06c4036ad54fe2b6e496f.png" />
+                    </Avatar>
+                    <p className="text-sm break-all">Quais informações você gostaria de saber sobre o time de {escolha}?</p>
+                </div>
+            </div>
+            <div className="flex items-center gap-2">
+                {infoTeam.map((item) => (
+                    <button onClick={() => handleInfoTeam(item)} key={item} className='text-sm break-all p-3 rounded-lg bg-amber-400 text-black hover:bg-amber-500 transition-all cursor-pointer mt-2'>
+                        {item}
+                    </button>
+                ))}
+            </div>
+            {infoTeamEscolhida && (
+                <div className="flex items-center gap-2 mt-2">
+                    <div className="flex items-center gap-2 p-3 rounded-lg bg-gray-800 mr-8">
+                        <p>{Array.isArray(infoTeamEscolhida) ? infoTeamEscolhida.join(', ') : infoTeamEscolhida}</p>
+                    </div>
+                </div>
+            )}
+        
+      </div>
+
+
+
+    )
+}
+
--- lading-page/src/components/chatbot.tsx
+++ lading-page/src/components/chatbot.tsx
@@ -1,129 +1,123 @@
 'use client'
+
+import { TeamData } from '@/models/TeamData';
 import { smoothScrollToElement } from '@/utils/smoothScroll';
-import { FormEvent, useEffect, useRef, useState } from 'react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect, useRef, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Chat } from './chat';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Button } from './ui/button';
+import { Form, FormControl, FormField, FormItem, FormMessage } from './ui/form';
 import { Input } from './ui/input';
 
-
-
-
 type Message = {
-    text: string;
-    isBot: boolean;
-  };
-  
-type TeamData = {
-    integrantes: string[];
-    titulos: string[];
-    situacao: string;
-    camisa: string;
+  text: string;
+  isBot: boolean;
 };
 
-export function ChatBot() {  
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [messages, setMessages] = useState<Message[]>([{text: 'Olá, eu sou o FURIA Chatbot. Como posso ajudar você hoje?', isBot: true}]);
-    const bottomRef = useRef<HTMLDivElement>(null);
-    useEffect(() => {
-      if (bottomRef.current) {
-        smoothScrollToElement(bottomRef.current, 1500);
-      }
-    }, [messages]);
-    
-    
-    const teamData: Record<string, TeamData> = {
-      valorant: {
-        integrantes: ['Khalil', 'mwzera', 'havoc', 'heat', 'raafa'],
-        titulos: ['Participação no Valorant Champions 2021 e 2022', 'Vaga permanente na liga internacional VALORANT desde 2022'],
-        situacao: 'Ativo na liga internacional - Temporada 2025',
-        camisa: 'https://furia.gg/store/valorant'
-      },
-      kingsleague: {
-        integrantes: ['Time draftado com 13 jogadores (10 selecionados em draft)'],
-        titulos: ['Participação na Kings League Brasil 2025'],
-        situacao: 'Em preparação para a primeira temporada',
-        camisa: 'https://furia.gg/store/kingsleague'
-      },
-      cs: {
-        integrantes: ['FalleN', 'chelo', 'yuurih', 'skullz', 'KSCERATO'],
-        titulos: [
-          'ESL Pro League Season 12 NA Champions',
-          '3º-4º no IEM Rio Major 2022',
-          'Semifinalista do PGL Major Antwerp 2022'
-        ],
-        situacao: 'Em reformulação com novos membros internacionais',
-        camisa: 'https://furia.gg/store/csgo'
-      },
-      rocketleague: {
-        integrantes: ['drufinho', 'Lostt', 'yanxnz'],
-        titulos: [
-          'Campeões do Gamers8 2022',
-          '3º-4º no RLCS 2022',
-          'MVP Sul-Americano (yanxnz)'
-        ],
-        situacao: 'Top 3 no ranking sul-americano',
-        camisa: 'https://furia.gg/store/rocketleague'
-      }
-    };
-  
-    const handleTeamQuery = (team: keyof typeof teamData) => {
-      const data = teamData[team];
-      const response = `
-        Integrantes: ${data.integrantes.join(', ')}
-        Títulos: ${data.titulos.join('\n      ')}
-        Situação Atual: ${data.situacao}
-        Camisas: ${data.camisa}
-      `;
-      setMessages([...messages, { text: response, isBot: true }]);
-    };
-  
-  function handleSubmit(event: FormEvent<HTMLFormElement>): void {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const userMessage = formData.get('message') as string;
-    console.log(userMessage);
-    setMessages([...messages, { text: userMessage, isBot: false }]);
+const formSchema = z.object({
+  message: z.string().min(1, { message: 'Mensagem é obrigatória' }),
+})
+
+export function ChatBot() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([{ text: 'Olá, eu sou o FURIA Chatbot. Como posso ajudar você hoje?', isBot: true }]);
+  const [escolha, setEscolha] = useState<string>('');
+  const bottomRef = useRef<HTMLDivElement>(null);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  })
+
+  function handleSubmit(values: z.infer<typeof formSchema>): void {
+    const userMessage = values.message;
+    if (escolha == '') {
+      setMessages([...messages, { text: userMessage, isBot: false }]);
+      form.reset();
+    } else {
+      setEscolha('');
+      setMessages([{ text: userMessage, isBot: false }]);
+      form.reset();
+    }
 
   }
-  
-    return (
-        <div className="fixed bottom-8 right-8 z-50">
+
+  useEffect(() => {
+    if (bottomRef.current) {
+      smoothScrollToElement(bottomRef.current, 1500);
+    }
+  }, [messages]);
+
+  const handleTeamQuery = (team: string) => {
+    setEscolha(team as keyof typeof TeamData);
+  };
+
+
+
+  return (
+    <div className="fixed bottom-8 right-8 z-50">
       <button
-        onClick={() => setIsOpen(!isOpen)}
+        onClick={() => {
+          setIsOpen(!isOpen);
+          setEscolha('');
+        }}
         className="bg-amber-400 text-black rounded-full p-4 shadow-lg hover:bg-amber-500 transition-all"
       >
         🗨️ Chat FURIA
       </button>
 
-      {isOpen && (
-        <div className="mt-4 bg-gray-900 rounded-lg shadow-xl w-96 overflow-hidden">
-          <div className="h-96 overflow-y-auto p-4 space-y-3 scroll-smooth">
-            {messages.map((msg, i) => (
-              <div
-                key={i}
-                className={`p-3 rounded-lg ${
-                  msg.isBot ? 'bg-gray-800 mr-8' : 'bg-amber-400 text-black ml-8'
-                }`}
-              >
 
+      {isOpen && (
+        <div className="mt-4 bg-gray-900 rounded-lg shadow-xl w-108 overflow-hidden">
+          <div className="h-96 overflow-y-auto p-4 space-y-3 scroll-smooth">
+            {escolha == '' ? (
+              messages.map((msg, i) => (
+                <div
+                  key={i}
+                  className={`p-3 rounded-lg ${msg.isBot ? 'bg-gray-800 mr-8' : 'bg-amber-400 text-black ml-8'
+                    }`}
+                >
                   <div className="flex items-center gap-2">
                     <Avatar>
                       <AvatarFallback>RD</AvatarFallback>
                       {msg.isBot ? (<AvatarImage src="https://cdn.dribbble.com/userupload/11627402/file/original-519eba43b5e06c4036ad54fe2b6e496f.png" />) : (
-                      <AvatarImage src="" />)}
-                      
+                        <AvatarImage src="" />)}
                     </Avatar>
                     <p className="text-sm break-all">{msg.text}</p>
                   </div>
-                
+                </div>
+              ))
+            ) : (
+              <div>
+
+                <Chat escolha={escolha} handleChat={() => { setEscolha(''); setMessages([{ text: 'Olá, eu sou o FURIA Chatbot. Como posso ajudar você hoje?', isBot: true }]) }} />
               </div>
-            ))}
-            <div ref={bottomRef} className="scroll-smooth"/>
+            )}
+            <div ref={bottomRef} className="scroll-smooth" />
           </div>
-          <form className='flex gap-2 p-4 border-t border-gray-700' onSubmit={handleSubmit}>
-            <Input name="message" type="text" placeholder="Pergunte o que quiser sobre a FURIA" />
-            <Button className='bg-amber-400 hover:bg-amber-500 text-black' type="submit">Enviar</Button>
-          </form>
+
+          <Form {...form}>
+            <form className='flex gap-2 p-4 border-t border-gray-700' onSubmit={form.handleSubmit(handleSubmit)}>
+              <FormField
+                control={form.control}
+                name="message"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input {...field} placeholder="Pergunte o que quiser sobre a FURIA" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button className='bg-amber-400 hover:bg-amber-500 text-black' type="submit">Enviar</Button>
+            </form>
+          </Form>
 
           <div className="grid grid-cols-2 gap-2 p-4 border-t border-gray-700">
             <button
@@ -155,5 +149,5 @@ export function ChatBot() {
       )}
     </div>
 
-    );
-  };
+  );
+};
