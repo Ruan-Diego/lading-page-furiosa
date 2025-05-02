e06ffd310bbfd2e7256ba88c3394a7cb041b703e
Ruan Diego <Ruandgn123@gmail.com>
2025-05-02 14:47:47 -0300

    fix: configurações de deplou

--- lading-page/next.config.ts
+++ lading-page/next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   images: {
+    unoptimized: true,
     domains: ['upload.wikimedia.org'],
   },
+  basePath: '/lading-page-furiosa',
+  assetPrefix: '/lading-page-furiosa/',
 };
 
 export default nextConfig;
