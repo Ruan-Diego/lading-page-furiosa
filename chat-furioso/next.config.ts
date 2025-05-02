127e19afeec2d69151615658c74207ea9ad4adaf
Ruan Diego <Ruandgn123@gmail.com>
2025-05-02 15:15:04 -0300

    fix: mmuda nome da pasta principal

--- chat-furioso/next.config.ts
+++ chat-furioso/next.config.ts
@@ -2,12 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  distDir: 'build', 
   images: {
     unoptimized: true,
     domains: ['upload.wikimedia.org'],
   },
-  basePath: '/lading-page-furiosa',
-  assetPrefix: '/lading-page-furiosa/',
+  basePath: '/chat-furioso',
+  assetPrefix: '/chat-furioso/',
 };
 
 export default nextConfig;
